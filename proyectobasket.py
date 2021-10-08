4057902c6ba4d24d210dd5fcdeaea76426d79493
soydanteprz <88898806+soydanteprz@users.noreply.github.com>
2021-10-08 14:10:41 -0500

    Add files via upload

--- proyectobasket.py
+++ proyectobasket.py
@@ -1,11 +1,6 @@
 # FUNCIONES
-
 def field_goals(t, g):
     return (g / t) * 100
-
-
-def prom_pg(a, b, c, d, e):
-    return (a++b+c+d+e)/5
 
 
 def match_points(pt,):
@@ -16,32 +11,33 @@ def match_points(pt,):
         acum = int(input())
         su = su + acum
         i = i + 1
-    return su/pt,
+    return su/pt
+
 
 def estatura_pies(length):
-    length*3.281
-    return length
+    return length*3.281
+
 
 def puntos2(i):
     total = 0
-    for i in range (101):
+    for i in range(101):
         total = total + i
     return 'sumatoria ', total
 
 
-
-# INTRODUCCION
-nombre = str(input('Ingrese su nombre '))
-apellido = str(input('Ingrese su apellido '))
-edad = int(input('Ingrese su edad '))
-peso = int(input('Ingrese su peso en Kg '))
-estatura = float(input('Ingrese su estatura en m '))
-
-estatura_pies(estatura)
-datos_persona = [nombre, apellido, edad, peso, estatura]
+#SOLITAR DATOS DEL USUARIO
+name = str(input('Ingrese su nombre '))
+surname = str(input('Ingrese su apellido '))
+ege = int(input('Ingrese su edad '))
+weight = int(input('Ingrese su peso en Kg '))
+lenght = float(input('Ingrese su estatura en m '))
+print("Tu estatura en pies es ", estatura_pies(lenght))
+datos_persona = [name, surname, ege, weight, lenght]
 print(datos_persona)
+
+#INTRODUCCION
 print("Bienvenido a tu programa de Basketball favorito")
-print("PRESIONA 1 - Estadísticas solo un partido\nPRESIONA 2 - Puntos por partido\nPRESIONA 3 - Field Goals\nPRESIONA 4 - Top 5 Mejores jugadores ")
+print("PRESIONA 1 - Estadísticas solo un partido\nPRESIONA 2 - Puntos por partido\nPRESIONA 3 - Field Goals\nPRESIONA 4 - Top 5 Leader Points")
 option = int(input("Elige la opción  "))
 
 # CONDICIONAL PARA SABER QUE SE  VA A OPERAR ES INDISPENSABLE
@@ -51,6 +47,7 @@ if option == 1:
     shots = int(input("Cuántos tiros hiciste?  "))
     goals = int(input("Cuántos anotaste?  "))
     fg = (goals * 100) / shots
+    match1 = [points, assist, shots, goals, fg]
     print("Tu porcentaje de tiros es de", fg, "%")
     print("Hoy metiste", points, "pts", assist, "ast")
 elif option == 2:
@@ -62,11 +59,11 @@ elif option == 3:
     print("Tu FG% es de", "%.2f" % field_goals(num1, num2), "%")
 elif option == 4:
 #USO DE FOR PARA RECORRER LA LISTA
-    best_players = ['Michael Jordan', 'Lebron James', 'Stephen Curry', 'James Harden', 'Giannis Antetokounmpo']
-    top=int(input('del 1 al 5 que jugador deseas ver'))
-    for top in best_players:
-        print(top)
+    best_players = ['Kareem Abdul-Jabbar', 'Karl Malone', 'Lebron James', 'Kobe Bryant', 'Michael Jordan']
+    players_points = [38387,36928,35367,33643,32292]
+    stats = [best_players, players_points]
+    print(stats)
+    for i in range(len(best_players)):
+        print('Jugador', best_players[i], '-', 'Puntos:', players_points[i])
 else:
     print('No tenemos esa opcion')
-
-
